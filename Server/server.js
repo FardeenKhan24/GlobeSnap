f9021b45fdac45c357080c68a412e43b8889149e
FardeenKhan24 <fardeenkhan200324@gmail.com>
2025-07-08 15:16:28 +0530

    Changed cors

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://globe-snap.vercel.app',
     credentials: true                
 }));
 app.use(express.json());
